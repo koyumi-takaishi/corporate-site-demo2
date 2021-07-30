faa55bd9916e9053e01f8f14dbec3967d30d09ac
koyumi-takaishi <takaishi.takoyaki@gmail.com>
2021-07-30 23:37:19 +0900

    0729

--- WordPressTheme/front-page.php
+++ WordPressTheme/front-page.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
 <div class="l-mv p-mv">
-  <div class="p-mv__img">
+  <picture class="p-mv__img">
+    <source srcset="<?php echo get_template_directory_uri() ?>/assets/img/common/kv-img.jpg" media="(min-width: 768px)"/>
     <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/kv-img-sp.jpg" alt="">
-  </div>
+  </picture>
   <div class="p-mv__block">
     <span class="p-mv__subtitle">デザインで人を笑顔にする会社</span>
     <span class="p-mv__subtitle--en">DIGSMILE INC.</span>
@@ -13,7 +14,7 @@
 </div>
 
 <section class="l-top-about p-about">
-  <div class="p-about__inner l-inner">
+  <div class="p-about__inner">
     <div class="p-about__content">
       <h2 class="c-section-title">about us</h2>
       <div class="p-about__box">
@@ -29,34 +30,36 @@
 </section>
 
 <div class="l-top-bgGray">
-  <section class="l-top-box1 p-box1">
-    <div class="p-box1__inner l-inner">
-      <h2 class="c-section-title">works</h2>
-      <div class="p-box1__img">
-        <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/works-img.jpg" alt="">
-      </div>
-      <div class="p-box1__body">
-        <p class="c-text">DIGSMILEの制作実績を紹介します。</p>
-        <div class="p-box1__btn">
-          <a href="#" class="c-btn">read more</a>
+  <div class="l-top-bgGray__inner">
+    <section class="l-top-box1 p-box1">
+      <div class="p-box1__inner l-inner">
+        <h2 class="c-section-title">works</h2>
+        <div class="p-box1__img">
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/works-img.jpg" alt="">
+        </div>
+        <div class="p-box1__body">
+          <p class="c-text">DIGSMILEの制作実績を紹介します。</p>
+          <div class="p-box1__btn">
+            <a href="#" class="c-btn">read more</a>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-  <section class="l-top-box1 p-box1">
-    <div class="p-box1__inner l-inner">
-      <h2 class="c-section-title">culture</h2>
-      <div class="p-box1__img">
-        <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/culture-img.jpg" alt="">
-      </div>
-      <div class="p-box1__body">
-        <p class="c-text">DIGSMILEの社内文化について紹介します。</p>
-        <div class="p-box1__btn">
-          <a href="#" class="c-btn">read more</a>
+    </section>
+    <section class="l-top-box1 p-box1">
+      <div class="p-box1__inner l-inner">
+        <h2 class="c-section-title">culture</h2>
+        <div class="p-box1__img">
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/culture-img.jpg" alt="">
+        </div>
+        <div class="p-box1__body">
+          <p class="c-text">DIGSMILEの社内文化について紹介します。</p>
+          <div class="p-box1__btn">
+            <a href="#" class="c-btn">read more</a>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </div>
 
 <section class="l-topics p-topics">
@@ -78,6 +81,24 @@
     </div>
     <div class="p-topics__btn">
       <a href="#" class="c-btn">read more</a>
+    </div>
+  </div>
+</section>
+
+<section class="l-contact p-contact">
+  <div class="l-inner">
+    <div class="p-contact__img">
+      <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/contact-img.jpg" alt="">
+    </div>
+    <div class="p-contact__body">
+      <h2 class="c-section-title">contact</h2>
+      <div class="p-contact__text">
+        <p class="c-text">制作の依頼、取材の依頼、IRや採用についての連絡・お問い合わせはコンタクトページから承っております。</p>
+        <p class="c-text">まずはお気軽にご連絡ください。担当者から改めて返信いたします。</p>
+      </div>
+      <div class="p-contact__btn">
+        <a href="#" class="c-btn">read more</a>
+      </div>
     </div>
   </div>
 </section>
